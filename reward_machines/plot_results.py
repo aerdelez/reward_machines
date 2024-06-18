82c1fdfd65fc0fee0e4d7ab04c1d9c618f18cea0
aerdelez <andro.erdelez@gmail.com>
2024-06-18 19:44:44 +0200

    plots readjusted

--- reward_machines/plot_results.py
+++ reward_machines/plot_results.py
@@ -3,14 +3,14 @@ import numpy as np
 import pandas as pd
 
 
-environments = ['mordor', 'mordor-single']
+environments = ['office', 'office-single']
 algorithms = ['crm', 'crm-rs', 'hrm', 'hrm-rs', 'ql', 'ql-rs']
 
 for noise in range(10, 99, 10):
     for environment in environments:
         for algorithm in algorithms:
             # Read the table
-            df = pd.read_csv(f'../mordor_noise_results/{noise}/summary/{environment}-{algorithm}.txt', delimiter='\t', 
+            df = pd.read_csv(f'../noise_results/{noise}/summary/{environment}-{algorithm}.txt', delimiter='\t', 
                              names=['timestep', '25th', 'median', '75th'])
 
             # Plot median reward as a line
@@ -24,14 +24,14 @@ for noise in range(10, 99, 10):
         plt.title(f'{environment} with {noise}% noise')
         plt.xlabel('Training steps (in thousands)')
         plt.ylabel('Avg. reward per step')
-        plt.ylim(0, 1.1)
-        plt.yticks([i / 10 for i in range(11)])
+        plt.ylim(0, 1.3)
+        plt.yticks([i / 10 for i in range(13)])
         plt.xlim(0, 100)
         plt.xticks([i for i in range(0, 101, 10)])
         plt.legend()
 
         # Save plot
-        plt.savefig(f'../mordor_noise_results/{noise}/plots/{environment}.png')
+        plt.savefig(f'../noise_results/{noise}/plots/{environment}.png')
         
         # Show plot
         plt.show()
