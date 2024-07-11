f743bf8e8c571e155112d93cdb60f46595a82ddf
aerdelez <andro.erdelez@gmail.com>
2024-07-11 17:55:32 +0200

    remove scripts and adjust some files

--- reward_machines/summarize_noise.py
+++ reward_machines/summarize_noise.py
@@ -8,6 +8,8 @@ algorithms = ['crm', 'crm-rs', 'hrm', 'hrm-rs', 'ql', 'ql-rs']
 
 for environment in environments:
     main_df = pd.DataFrame(columns=algorithms)
+    df_25th = pd.DataFrame(columns=algorithms)
+    df_75th = pd.DataFrame(columns=algorithms)
     for algorithm in algorithms:
         if 'mordor' in environment:
             # Read the table
@@ -16,7 +18,10 @@ for environment in environments:
         else:
             df = pd.read_csv(f'../test_results/summary/{environment}-{algorithm}.txt', delimiter='\t', 
                             names=['timestep', '25th', 'median', '75th'])
+            
         main_df.loc[0, algorithm] = df.iloc[-1].to_dict()['median']
+        df_25th.loc[0, algorithm] = float(df.iloc[-1]['25th'])
+        df_75th.loc[0, algorithm] = float(df.iloc[-1]['75th'])
     for noise in range(10, 99, 10):
         for algorithm in algorithms:
             if 'mordor' in environment:
@@ -29,13 +34,15 @@ for environment in environments:
                                 names=['timestep', '25th', 'median', '75th'])
             
             main_df.loc[noise // 10, algorithm] = df.iloc[-1].to_dict()['median']
+            df_25th.loc[noise // 10, algorithm] = float(df.iloc[-1]['25th'])
+            df_75th.loc[noise // 10, algorithm] = float(df.iloc[-1]['75th'])
 
            
     # Plotting the data
     plt.figure(figsize=(10, 6))
-    
     for algorithm in algorithms:
         plt.plot(main_df.index * 10, main_df[algorithm], marker='o', label=algorithm.upper())
+        plt.fill_between(df_25th.index * 10, df_25th[algorithm], df_75th[algorithm], alpha=0.2)
     
     plt.xlabel('Noise level')
     plt.ylabel('Avg. reward in the 100000th step')
